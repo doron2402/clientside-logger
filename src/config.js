f236cb0c9b80a0cb1551e35488e0f0644e6b1892
Doron Segal <doron2402@gmail.com>
2015-06-08 19:23:51 +0100

    adding settings

--- src/config.js
+++ src/config.js
@@ -1,13 +1,11 @@
 'use strict';
 
-var config;
-
-config = {
-  development: require(__BASE + '/configs/development'),
-  'ci-uswest2': require(__BASE + '/configs/ci-uswest2'),
-  'pp-uswest2': require(__BASE + '/configs/pp-uswest2'),
-  'prod-euwest1': require(__BASE + '/configs/prod-euwest1'),
-  'prod-uswest2': require(__BASE + '/configs/prod-uswest2')
+var settings = {
+  development: require(__BASE + '/settings/development'),
+  'ci-uswest2': require(__BASE + '/settings/ci-uswest2'),
+  'pp-uswest2': require(__BASE + '/settings/pp-uswest2'),
+  'prod-euwest1': require(__BASE + '/settings/prod-euwest1'),
+  'prod-uswest2': require(__BASE + '/settings/prod-uswest2')
 };
 
-module.exports = process.env.NODE_ENV ? config[process.env.NODE_ENV] : config.development;
+module.exports = process.env.NODE_ENV ? settings[process.env.NODE_ENV] : settings.development;
